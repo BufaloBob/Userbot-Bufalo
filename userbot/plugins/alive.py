307ab97bfc69aab3e6031e21d6ec2b412f9f6528
BufaloBob <64494044+BufaloBob@users.noreply.github.com>
2020-05-15 09:57:33 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,8 +12,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "unknown"
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**🎲 __THE_KILLER_BOB USERBOT STATUS__ 🎲**\n\n"
-                     "⭐️Tetethon Version: 7.0.1\n⭐️Python Version: 3.8.0\n🥶**SUPPORT & UPDATE: I CAN SUPPORT YOU **\n\n"
-                     "🥶**CUSTOM BOT CREATOR:** [The_Killer_Bob](tg://user?id=) **\n\n"
+                     "⭐️Tetethon Version: 7.0.1\n\n⭐️Python Version: 3.8.0\n\n🥶**SUPPORT & UPDATE: I CAN SUPPORT YOU **\n\n"
+                     "🥶**Custom By:** [The_Killer_Bob](tg://user?id=) **\n\n"
                      "⭐️**CPU:** Ok \n\n🎲 **__USER DATA__** 🎲\n\n"
                      f"🥶 **USER:** __{DEFAULTUSER}__\n\n"
                      "🥶[SHOPPY](https://shoppy.gg/@The_Killer_Bob)")
