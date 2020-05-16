9020d98ceb0f9e138327639e05ab4abb69ab7822
BufaloBob <64494044+BufaloBob@users.noreply.github.com>
2020-05-16 21:29:39 +0200

    Add files via upload

---
+++ userbot/plugins/members.py
@@ -0,0 +1,22 @@
+from telethon.tl.functions.channels import InviteToChannelRequest
+import os
+
+@command(pattern=r"^.steal ?(.*)?")
+async def steal(event):
+    await event.edit("Processing...")
+    to_channel = os.environ.get("TO_CHANNEL")
+    if to_channel is None:
+        await event.edit("Please setup your channel in TO_CHANNEL")
+        return
+    to_channel = to_channel if not to_channel.isdigit() else int(to_channel)
+    input_str = event.pattern_match.group(1)
+    input_str = "" if input_str is None else input_str
+    input_str = int(input_str) if input_str.isdigit() else input_str
+    if not input_str:
+        input_str = event.chat_id
+    async for user in bot.iter_participants(input_str, aggressive=True):
+        try:
+            await bot(InviteToChannelRequest(to_channel, [user.id]))
+        except:
+            continue
+    await event.edit("Finish.")
