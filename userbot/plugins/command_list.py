5dc14319b615503628ae0093ba1c366bdcc0321a
BufaloBob <64494044+BufaloBob@users.noreply.github.com>
2020-05-25 10:59:49 +0200

    Update command_list.py

--- userbot/plugins/command_list.py
+++ userbot/plugins/command_list.py
@@ -16,5 +16,5 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"**List of Plugins:**\n{o}\n\n**TIP:** 𝑰𝒇 𝒚𝒐𝒖 𝒘𝒂𝒏𝒕 𝒕𝒐 𝒌𝒏𝒐𝒘 𝒕𝒉𝒆 𝒄𝒐𝒎𝒎𝒂𝒏𝒅𝒔 𝒇𝒐𝒓 𝒂 𝒑𝒍𝒖𝒈𝒊𝒏, 𝒅𝒐:- \n .𝒉𝒆𝒍𝒑 <𝒑𝒍𝒖𝒈𝒊𝒏 𝒏𝒂𝒎𝒆>` **𝒘𝒊𝒕𝒉𝒐𝒖𝒕 𝒕𝒉𝒆 < > 𝒃𝒓𝒂𝒄𝒌𝒆𝒕𝒔.**\n 𝑨𝒍𝒍 𝒑𝒍𝒖𝒈𝒊𝒏𝒔 𝒎𝒊𝒈𝒉𝒕 𝒏𝒐𝒕 𝒘𝒐𝒓𝒌 𝒅𝒊𝒓𝒆𝒄𝒕𝒍𝒚."
+    OUTPUT = f"**List of Plugins:**\n{o}\n\n**TIP:** __𝑰𝒇 𝒚𝒐𝒖 𝒘𝒂𝒏𝒕 𝒕𝒐 𝒌𝒏𝒐𝒘 𝒕𝒉𝒆 𝒄𝒐𝒎𝒎𝒂𝒏𝒅𝒔 𝒇𝒐𝒓 𝒂 𝒑𝒍𝒖𝒈𝒊𝒏, 𝒅𝒐:-__ \n `.help <plugin name>` **𝒘𝒊𝒕𝒉𝒐𝒖𝒕 𝒕𝒉𝒆 < > 𝒃𝒓𝒂𝒄𝒌𝒆𝒕𝒔.**\n__𝑨𝒍𝒍 𝒑𝒍𝒖𝒈𝒊𝒏𝒔 𝒎𝒊𝒈𝒉𝒕 𝒏𝒐𝒕 𝒘𝒐𝒓𝒌 𝒅𝒊𝒓𝒆𝒄𝒕𝒍𝒚"
     await event.edit(OUTPUT)
