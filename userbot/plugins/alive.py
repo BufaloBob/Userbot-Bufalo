a7502c03ec9e2b20e285599c99f4a417b278aac5
BufaloBob <64494044+BufaloBob@users.noreply.github.com>
2020-05-29 10:53:16 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,19 +1,41 @@
 """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+
 import asyncio
+
 from telethon import events
+
 from telethon.tl.types import ChannelParticipantsAdmins
+
 from platform import uname
+
 from userbot import ALIVE_NAME
+
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "unknown"
 
-@command(outgoing=True, pattern="^.alive$")
+
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
+
+
+
+#@command(outgoing=True, pattern="^.alive$")
+
+@borg.on(admin_cmd(pattern=r"alive"))
+
 async def amireallyalive(alive):
+
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**🎲 __THE_KILLER_BOB USERBOT STATUS__ 🎲**\n\n"
-                     "⭐️Tetethon Version: 7.0.1\n\n⭐️Python Version: 3.8.0\n\n🥶**SUPPORT & UPDATE: I CAN SUPPORT YOU **\n\n"
-                     "🥶**Custom By:** [The_Killer_Bob](tg://user?id=) **\n\n"
-                     "⭐️**CPU:** Ok \n\n🎲 **__USER DATA__** 🎲\n\n"
-                     f"🥶 **USER:** __{DEFAULTUSER}__\n\n"
-                     "🥶[SHOPPY](https://shoppy.gg/@The_Killer_Bob)")
+
+    await alive.edit("░█─░█ █▀▀ █── █── █▀▀█ \n░█▀▀█ █▀▀ █── █── █──█ \n░█─░█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ \n\nYEEEE FINALLY ! I'm Alive\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n"
+
+                     f"My BOSS`: {DEFAULTUSER}\n"
+
+                     "⭐️Tetethon Version: 7.0.1\n\n⭐️Python Version: 3.8.0\n\n🥶**MORE INFO**\n"
+					 
+					 "⭐️**CPU:** Ok \n"
+
+                     "Database Status: Databases functioning normally!\n\nAlways with you, my Boss!\n"
+
+                     "[Contact My Boss Now](https://t.me/The_Killer_Bob)")
