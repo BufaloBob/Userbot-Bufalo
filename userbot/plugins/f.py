2fedda9478f15b78b7f09ad135b7d682e21798d2
BufaloBob <64494044+BufaloBob@users.noreply.github.com>
2020-05-30 09:53:54 +0200

    Create f.py

---
+++ userbot/plugins/f.py
@@ -0,0 +1,52 @@
+"""Emoji
+
+Available Commands:
+
+.emoji shrug
+
+.emoji apple
+
+.emoji :/
+
+.emoji -_-"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 117)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "f":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+        
+
+            "️┏━━━┓\n┃┏━━┛\n┃┗━┓\n┃┏━┛\n┃┃\n┗┛ \n\n\PRESS F TO PAY RESPECT" 
+            
+            await asyncio.sleep(5)
+            
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 117])
